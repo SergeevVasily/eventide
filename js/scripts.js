98baf6e64e056363ed4e3d2f16958d3a1de1ff10
Vasily Sergeev <vasikanava@gmail.com>
2019-01-07 22:31:22 -0800

    form validation

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,7 @@ $(function () {
    $('.collapse').on('shown.bs.collapse', function () {
        $(this).prev().find('.fa').removeClass().addClass('fas fa-minus-circle')
    });
+
+   $('#myForm').validator()
 });
 
